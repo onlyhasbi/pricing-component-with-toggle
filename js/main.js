816cfe10ac926124168b952ff1381c63490d3d71
A MUH HASBI H <onlyhasbi@gmail.com>
2022-05-10 04:50:01 +0800

    complete css and javascript

--- js/main.js
+++ js/main.js
@@ -1,42 +1,13 @@
-const data = [
-  {
-    name: "basic",
-    annually: 199.99,
-    monthly: 19.99,
-  },
-  {
-    name: "professional",
-    annually: 249.99,
-    monthly: 24.99,
-  },
-  {
-    name: "master",
-    annually: 399.99,
-    monthly: 39.99,
-  },
-];
+var pricingToggle = document.querySelector("#pricing");
+var annuallyPrice = document.querySelectorAll(".annually");
+var monthlyPrice = document.querySelectorAll(".monthly");
 
-function annually() {
-  var basic = document.querySelector(".basic");
-  var proffesional = document.querySelector(".proffesional");
-  var master = document.querySelector(".master");
-
-  basic.textContent = data[0].annually;
-  proffesional.textContent = data[1].annually;
-  master.textContent = data[2].annually;
-}
-
-function monthly() {
-  var basic = document.querySelector(".basic");
-  var proffesional = document.querySelector(".proffesional");
-  var master = document.querySelector(".master");
-
-  basic.textContent = data[0].monthly;
-  proffesional.textContent = data[1].monthly;
-  master.textContent = data[2].monthly;
-}
-
-const monthlyPlan = document.querySelector("#toggle");
-monthlyPlan.addEventListener("change", (e) => {
-  e.currentTarget.checked ? monthly() : annually();
+pricingToggle.addEventListener("change", (e) => {
+  if (e.currentTarget.checked) {
+    annuallyPrice.forEach((item) => item.setAttribute("hidden", true));
+    monthlyPrice.forEach((item) => item.removeAttribute("hidden"));
+  } else {
+    monthlyPrice.forEach((item) => item.setAttribute("hidden", true));
+    annuallyPrice.forEach((item) => item.removeAttribute("hidden"));
+  }
 });
